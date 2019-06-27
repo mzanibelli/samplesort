c9276833560e3ad5433d688756eec65d1e5bb2d6
Matéo Zanibelli <mateo@occitech.fr>
2019-06-27 08:24:24 +0200

    Refactor sample tests

--- sample/sample_test.go
+++ sample/sample_test.go
@@ -6,72 +6,69 @@ import (
 	"testing"
 )
 
-func TestSample(t *testing.T) {
-	t.Run("it should flatten data",
-		func(t *testing.T) {
-			input := map[string]interface{}{
-				"foo": map[string]interface{}{
-					"bar": 0.001,
-				},
-			}
-			expected := []float64{0.001}
-			SUT := sample.New("")
-			SUT.Flatten(input)
-			actual := SUT.Values()
-			if !reflect.DeepEqual(expected, actual) {
-				t.Errorf("expected: %v, actual: %v", expected, actual)
-			}
-		})
-	t.Run("it should extract floats",
-		func(t *testing.T) {
-			input := map[string]interface{}{
-				"foo": map[string]interface{}{
-					"bar": 0.001,
-					"baz": "hello",
-				},
-			}
-			expected := []float64{0.001}
-			SUT := sample.New("")
-			SUT.Flatten(input)
-			actual := SUT.Values()
-			if !reflect.DeepEqual(expected, actual) {
-				t.Errorf("expected: %v, actual: %v", expected, actual)
-			}
-		})
-	t.Run("it should sort features by key",
-		func(t *testing.T) {
-			input := map[string]interface{}{
-				"zzz": 4.,
-				"foo": map[string]interface{}{
-					"bbb": 2.,
-					"zzz": 3.,
-					"aaa": 1.,
-				},
-			}
-			expected := []float64{1, 2, 3, 4}
-			SUT := sample.New("")
-			SUT.Flatten(input)
-			actual := SUT.Values()
-			if !reflect.DeepEqual(expected, actual) {
-				t.Errorf("expected: %v, actual: %v", expected, actual)
-			}
-		})
-	t.Run("it should return sorted keys on demand",
-		func(t *testing.T) {
-			input := map[string]interface{}{
-				"zzz": 4.,
-				"foo": map[string]interface{}{
-					"bbb": 2.,
-					"zzz": 3.,
-					"aaa": 1.,
-				},
-			}
-			expected := []string{"foo.aaa", "foo.bbb", "foo.zzz", "zzz"}
-			SUT := sample.New("")
-			SUT.Flatten(input)
-			actual := SUT.Keys()
-			if !reflect.DeepEqual(expected, actual) {
-				t.Errorf("expected: %v, actual: %v", expected, actual)
-			}
-		})
+func TestItShouldFlattenData(t *testing.T) {
+	input := map[string]interface{}{
+		"foo": map[string]interface{}{
+			"bar": 0.001,
+		},
+	}
+	expected := []float64{0.001}
+	SUT := sample.New("")
+	SUT.Flatten(input)
+	actual := SUT.Values()
+	if !reflect.DeepEqual(expected, actual) {
+		t.Errorf("expected: %v, actual: %v", expected, actual)
+	}
+}
+
+func TestItShouldExtractFloats(t *testing.T) {
+	input := map[string]interface{}{
+		"foo": map[string]interface{}{
+			"bar": 0.001,
+			"baz": "hello",
+		},
+	}
+	expected := []float64{0.001}
+	SUT := sample.New("")
+	SUT.Flatten(input)
+	actual := SUT.Values()
+	if !reflect.DeepEqual(expected, actual) {
+		t.Errorf("expected: %v, actual: %v", expected, actual)
+	}
+}
+
+func TestItShouldSortValuesByKey(t *testing.T) {
+	input := map[string]interface{}{
+		"zzz": 4.,
+		"foo": map[string]interface{}{
+			"bbb": 2.,
+			"zzz": 3.,
+			"aaa": 1.,
+		},
+	}
+	expected := []float64{1, 2, 3, 4}
+	SUT := sample.New("")
+	SUT.Flatten(input)
+	actual := SUT.Values()
+	if !reflect.DeepEqual(expected, actual) {
+		t.Errorf("expected: %v, actual: %v", expected, actual)
+	}
+}
+
+func TestItShouldSortKeysByKey(t *testing.T) {
+	input := map[string]interface{}{
+		"zzz": 4.,
+		"foo": map[string]interface{}{
+			"bbb": 2.,
+			"zzz": 3.,
+			"aaa": 1.,
+		},
+	}
+	expected := []string{"foo.aaa", "foo.bbb", "foo.zzz", "zzz"}
+	SUT := sample.New("")
+	SUT.Flatten(input)
+	actual := SUT.Keys()
+	if !reflect.DeepEqual(expected, actual) {
+		t.Errorf("expected: %v, actual: %v", expected, actual)
+	}
 }
